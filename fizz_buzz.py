428d61c1f633ab86b7c3a5621d5debe74c343321
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-26 23:34:59 -0700

    Update fizz_buzz.py
    changed i to num and added a comment

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,10 +1,12 @@
-#fizz_buzz.py: Solving the Fizz_Buzz question
-for i in range(1,101):
-  if i % 3 == 0 and i % 5 != 0:
+#fizz_buzz.py: Solving the FizzBuzz question
+
+#for loop generator from 1-100
+for num in range(1,101):
+  if num % 3 == 0 and num % 5 != 0:
     print("Fizz")
-  elif i % 5 == 0 and i % 3 != 0:
+  elif num % 5 == 0 and num % 3 != 0:
     print("Buzz")
-  elif i % 3 == 0 and i % 5 == 0:
+  elif num % 3 == 0 and num % 5 == 0:
     print("FizzBuzz")
   else:
-    print(i)
+    print(num)
